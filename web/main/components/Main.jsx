7fc2e2abbbae294771067301aab9048fc13f109b
Seongbae103 <seongbae103@gmail.com>
2022-12-07 20:59:12 +0900

    12-07

--- web/main/components/Main.jsx
+++ web/main/components/Main.jsx
@@ -1,4 +1,3 @@
-import {Nav} from 'web/main'
 import "web/main/components/Main.css"
 
 const Main = ()=> {
@@ -6,7 +5,6 @@ const Main = ()=> {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossOrigin="anonymous"/>
     
     <table className="container-fluid">
-        <Nav/>
         <thead></thead>
         <tbody>
             <tr id='service' style={{height: "1080px"}}>service</tr>
--- web/main/components/Nav.jsx
+++ web/main/components/Nav.jsx
@@ -1,10 +1,11 @@
 import { Link } from "react-router-dom"
 
+
 const Nav=()=>{
     return(<>
     <nav className="navbar navbar-expand-lg gnav">
         <div className="container-fluid">
-            <a className="navbar-brand" href="#">Borrow-San</a>
+            <a className="navbar-brand" href ="/main">Borrow-San</a>
                 
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav me-auto mb-2 mb-lg-0">
@@ -14,11 +15,11 @@ const Nav=()=>{
                 <li className="nav-item">
                     <a className="nav-link" href="#howto">이용방법</a>
                 </li>
-                <li >
-                    <Link to ="/notice" className="nav-item">공지사항</Link>
+                <li className="nav-item">
+                    <Link to ="/notice" className="nav-link">공지사항</Link>
                 </li>
-                <li >
-                    <Link to ="/mn" className="nav-item">관리자</Link>
+                <li className="nav-item">
+                    <Link to ="/mn" className="nav-link">관리자</Link>
                 </li>
             </ul>
             </div>
