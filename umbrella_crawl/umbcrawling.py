00b1a7d2283aed92f2030496d6265b790a69e62c
Seongbae103 <seongbae103@gmail.com>
2023-01-09 20:33:32 +0900

    crawl

--- umbrella_crawl/umbcrawling.py
+++ umbrella_crawl/umbcrawling.py
@@ -1,27 +1,25 @@
-import os
-import sys
-
 import numpy as np
+from matplotlib import pyplot as plt
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
 import urllib.request
+import os
+from tensorflow import keras
+os.environ['KMP_DUPLICATE_LIB_OK']='True'
 
-from tensorflow_datasets.object_detection.open_images_challenge2019_beam import cv2
+class Crawling(object):
+    def __init__(self):
+        global search, count, save, img_name
+        img_name = "파손_영어"
+        search = "broken umbrella"
+        count = 10000  # 크롤링할 이미지 개수
+        save = r"C:\Users\AIA\PycharmProjects\borrow_sub\umbrella_crawl\data\broken_umb_eng"
 
-
-class Model(object):
-    def crawling(self):
-        search = "우산"   # 이미지 이름
-        count = 101    # 크롤링할 이미지 개수
-        saveurl = r"C:\Users\seongbae\Desktop\borrow_sub\umbrella_crawl\save\umb"  # 이미지들을 저장할 폴더 주소
-
-        ## 셀레니움으로 구글 이미지 접속 후 이미지 검색
-
+    def image_crawling(self):
         options = webdriver.ChromeOptions()
         options.headless = True
         options.add_argument("window-size=1920x1080")
-
         driver = webdriver.Chrome(options=options)  #options=options
         driver.get("https://www.google.co.kr/imghp?hl=ko&tab=wi&ogbl")
         elem = driver.find_element_by_name("q")
@@ -37,7 +35,6 @@ class Model(object):
         # 스크롤 끝까지 내리기
 
         while True:
-
             driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
             # 페이지 로딩 기다리기
             time.sleep(SCROLL_PAUSE_TIME)
@@ -46,12 +43,9 @@ class Model(object):
             new_height = driver.execute_script("return document.body.scrollHeight")
 
             if new_height == last_height:
-
-                try:
-                    driver.find_element_by_css_selector(".mye4qd").click()
-
-                except:
-                    break
+                driver.find_element_by_css_selector(".mye4qd").click()
+            else:
+                break
 
             last_height = new_height
 
@@ -59,33 +53,16 @@ class Model(object):
         images = driver.find_elements_by_css_selector(".rg_i.Q4LuWd")
 
         for i in range(count):
+            images[i].click() # 이미지 클릭
+            time.sleep(10)
 
-            try:
-                images[i].click() # 이미지 클릭
-                time.sleep(1)
+            imgUrl = driver.find_element_by_css_selector(".n3VNCb").get_attribute("src")
+            urllib.request.urlretrieve(imgUrl, save + '/'+ img_name + str(i) + ".jpg")    # 이미지 다운
 
-                imgUrl = driver.find_element_by_css_selector(".n3VNCb").get_attribute("src")
-
-                src = cv2.imread(imgUrl, cv2.IMREAD_GRAYSCALE)
-
-                if src is None:
-                    print('Image load failed!')
-                    sys.exit()
-                edges = cv2.Canny(src, 50, 150)
-                lines = cv2.HoughLinesP(edges, 1.0, np.pi / 180, 160, minLineLength=50, maxLineGap=5)
-                dst = cv2.cvtColor(edges, cv2.COLOR_GRAY2BGR)
-
-                if lines is not None:
-                    for i in range(lines.shape[0]):  # range(N)
-                        pt1 = (lines[i][0][0], lines[i][0][1])
-                        pt2 = (lines[i][0][2], lines[i][0][3])
-                        cv2.line(dst, pt1, pt2, (0, 0, 255), 2, cv2.LINE_AA)
-
-                urllib.request.urlretrieve(imgUrl, saveurl + str(i) + ".jpg")    # 이미지 다운
-                cv2.waitKey()
-            except:
-                pass
         driver.close()
 
+
+
+
 if __name__ == '__main__':
-    Model().crawling()
+    Crawling().image_crawling()
